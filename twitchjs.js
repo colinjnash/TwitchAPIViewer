de1b1aee39b988d858ebc7aae8f4a03ea9375483
Colin Nash <colinjnash@gmail.com>
2017-08-15 21:18:40 -0400

    Add files via upload

--- twitchjs.js
+++ twitchjs.js
@@ -1,64 +1,44 @@
 $(document).ready(function() {
 
+    "use strict";
 
-    var chanList = ["ESL_SC2", "OgamingSC2", "luminosity", "giantwaffle", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", ];
-var online = [];
-var offline = [];
-
-
-function onlineOffline() {
-
-	chanList.forEach(function(name) {
-
-		var streams = 'https://wind-bow.gomix.me/twitch-api/streams/' + name;
-
-	    $.ajax({
-			url: streams,
-			type: 'GET',
-			dataType: 'JSONP',
-			
-			
-		})
-		.success(function(obj) {
-		if (obj.stream !==null) {
-			online.push(name);
-		
-		}
-		else offline.push(name);
-
-		});
-		
-});
-}
+    var chanList = ["ESL_SC2", "OgamingSC2", "luminosity", "giantwaffle", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb"];
+    var online = [];
+    var offline = [];
 
 
+    function onlineOffline() {
 
+        chanList.forEach(function(name) {
+
+            var streams = 'https://wind-bow.gomix.me/twitch-api/streams/' + name;
+
+            $.ajax({
+                    url: streams,
+                    type: 'GET',
+                    dataType: 'JSONP',
+
+                })
+                .success(function(obj) {
+                    if (obj.stream !== null) {
+                        online.push(name);
+
+                    } else offline.push(name);
+
+                });
+
+
+        });
+    }
+
+console.log(online);
 
     function getInfo() {
 
+        chanList.forEach(function(name) {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-        online.forEach(function(name) {
-
-
-
-        	console.log(name);
-        	
-
-
-
+         
+            console.log(name);
 
 
             $.ajax({
@@ -66,26 +46,16 @@ function onlineOffline() {
                     type: 'GET',
                     dataType: 'JSONP',
 
-
-
-
                 })
                 .success(function(data) {
                     console.log("success");
                     if (data.logo === null) {
-     	data.logo = "http://www.freeiconspng.com/uploads/no-image-icon-32.png";
-     	
-     }
+                        data.logo = "http://www.freeiconspng.com/uploads/no-image-icon-32.png";
+
+                    }
 
                     $('.online').append(
                         '<div class="username" id="' + data.display_name + '"><img class="logo" src="' + data.logo + '"></img><p class="userinput">' + data.display_name + '</p></div>');
-
-
-
-
-
-
-
 
 
                 });
@@ -96,27 +66,7 @@ function onlineOffline() {
 
     }
 
-onlineOffline();
-console.log(online);
-console.log(offline);
-
+    onlineOffline();
     getInfo();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 });
